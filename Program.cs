b2e47fc281575947132358ab2e2dc28e6a211b23
vuogtrinh <127485423+vuogtrinh@users.noreply.github.com>
2023-03-10 16:39:57 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,221 +1,192 @@
-﻿/*- Thực hiện phép SS để xác định và in ra màn hình các thông tin
-của xạ thủ có trình độ cao hơn
-- Với xạ thủ có trình độ cao hơn, được phép chuyển đổi giới tính
-Với xạ thủ có trình độ thấp hơn thì giới tính là nam
-- Với trình độ bậc 5 và giới tính là nam được phép sử dụng Súng máy
-Với trình độ bậc 5 là nữ thì được phép sử dụng tiểu liên
-In ra màn hình 2 xạ thủ này.
-Từ dữ liệu của bài trước,  các em tạo mảng các xạ thủ là các bạn trong lớp với các yêu cầu:
-- Chuẩn hóa tên của các xạ thủ
-- Xạ thủ có tên trong các tên: Dũng, Quý,  Trâm,  Bằng Anh thì có trình độ 5.
-Các xạ thủ còn lại các em tự đánh giá
-*/
-using System;
-using System.Xml.Linq;
+﻿using System;
+using System.ComponentModel;
 
-namespace BtvnBuoi4
+namespace KiemTra1
 {
     internal class Program
     {
-        struct XaThu
+        struct SinhVien
         {
             public string HoTen;
             public string GioiTinh;
-            public TrinhDo TD;
-            public LoaiSung LS;
-            public double Diem;
+            public string Que;
+            public string HocLuc;
         }
-        enum TrinhDo
-        {
-            LinhMoi = 1,
-            BinhNhat = 2,
-            DoiTruong = 3,
-            ChuyenNghiep = 4,
-            ChuyenGia = 5,
-
-        }
-        struct LoaiSung
-        {
-            public string TenSung;
-            public double TyLe;
-            public double CuLy;
-            public double TocDo;
-        }
-
-
         static void Main(string[] args)
         {
-            int n;
-            int i;
+            int n,i;
 
-            Console.Write("Nhap so xa thu: ");
+            Console.Write("Nhap so sinh vien: ");
             n = int.Parse(Console.ReadLine());
-            XaThu[] xt = new XaThu[n];
 
-            Console.WriteLine("\n*Nhap thong tin xa thu*");
-            for (i = 0; i < n; i++)
+            SinhVien[] sv = new SinhVien[n];
+
+            for(i=0;i<n;i++)
             {
-                Console.Write("Nhap ten xa thu {0}: ", i + 1);
+                Console.Write("\nNhap ten sinh vien {0}: ", i+1);
                 string name = Console.ReadLine();
-                xt[i].HoTen = name;
+                sv[i].HoTen = name;
 
                 Console.Write("Nhap gioi tinh: ");
                 string gender = Console.ReadLine();
-                xt[i].GioiTinh = gender;
+                sv[i].GioiTinh = gender;
+                
+                Console.Write("Nhap que quan: ");
+                string que = Console.ReadLine();
+                sv[i].Que = que;
 
-                Console.Write("Nhap trinh do: ");
-                int skill = Convert.ToInt32(Console.ReadLine());
-                xt[i].TD = (TrinhDo)skill;
-
-                Console.Write("Nhap ten sung: ");
-                string gn = Console.ReadLine();
-                xt[i].LS.TenSung = gn;
-
-                Console.Write("Nhap ty le: ");
-                double acc = Convert.ToDouble(Console.ReadLine());
-                xt[i].LS.TyLe = acc;
-
-                Console.Write("Nhap cu ly: ");
-                double dis = Convert.ToDouble(Console.ReadLine());
-                xt[i].LS.CuLy = dis;
-
-                Console.Write("Nhap toc do: ");
-                double spd = Convert.ToDouble(Console.ReadLine());
-                xt[i].LS.TocDo = spd;
-
-                Console.Write("Diem: ");
-                double pt = Convert.ToDouble(Console.ReadLine());
-                xt[i].Diem = pt;
-                Console.Write("\n");
+                Console.Write("Nhap hoc luc: ");
+                string hocluc = Console.ReadLine();
+                sv[i].HocLuc = hocluc;
             }
-            
-            //Chuan hoa ten xa thu
-            for(i= 0; i<n; i++)
+
+            //Chuan hoa ho ten va que quan
+            for (i = 0; i < n; i++)
             {
-                //xoa dau cach thua o dau va cuoi
-                xt[i].HoTen.Trim();
-                //chuyen xau ve dang viet thuong
-                xt[i].HoTen.ToLower();
-                //kiem tra xem co 2 dau cach lien nhau khong
-                while (xt[i].HoTen.IndexOf("  ") != -1)
+                sv[i].HoTen.Trim();
+
+                sv[i].HoTen.ToLower();
+
+                while (sv[i].HoTen.IndexOf("  ") != -1)
                 {
-                    //xoa bot 1 dau cach neu thua
-                    xt[i].HoTen.Remove(xt[i].HoTen.IndexOf("  "), 1);
+                    sv[i].HoTen.Remove(sv[i].HoTen.IndexOf("  "), 1);
                 }
-                //viet hoa chu cai dau tien
-                //tach cac tu boi dau cach
-                string[] s = xt[i].HoTen.Split(' ');
-                //bien luu tru ky tu sau khi chuan hoa
-                string afterFormat = "";
-                //thuc hien vong lap len toan bo phan tu sau khi tach
+
+                string[] s = sv[i].HoTen.Split(' ');
+
+                string afterFormatn = "";
+
                 for (int j = 0; j < s.Length; ++j)
                 {
-                    //lay ra ky tu dau tien
-                    string first = s[j].Substring(0, 1);
-                    //lay ra xau ki tu sau chu cai dau tien
-                    string another = s[j].Substring(1, s[j].Length - 1);
-                    //viet lai xau sau khi chuan hoa
-                    afterFormat += first.ToUpper() + another + " ";
+                    string firstn = s[j].Substring(0, 1);
+                    string anothern = s[j].Substring(1, s[j].Length - 1);
+                    afterFormatn += firstn.ToUpper() + anothern + " ";
                 }
-                //xoa dau cach o cuoi sau khi chuan hoa
-                xt[i].HoTen = afterFormat.Remove(afterFormat.LastIndexOf(' '), 1);
-                //cac xa thu co ten chuyen thanh trinh do 5
-                if (xt[i].HoTen.Contains("Dung") || xt[i].HoTen.Contains("Quy") || xt[i].HoTen.Contains("Tram") || xt[i].HoTen.Contains("Bang Anh"))
-                    xt[i].TD = TrinhDo.ChuyenGia;
+                sv[i].HoTen = afterFormatn.Remove(afterFormatn.LastIndexOf(' '), 1);
             }
-            /*
-            //Doi gioi tinh cho xa thu trinh do 5
-            Console.WriteLine("\n*Doi gioi tinh cho xa thu trinh do 5*");
-            char checkgender;
-            int max=0;
+
+            //in thong tin sinh vien
             for(i = 0; i<n; i++)
             {
-                xt[max].TD = xt[0].TD;
-                if (xt[max].TD == TrinhDo.ChuyenGia)
-                {
-                    Console.Write("Ban co muon doi gioi tinh?(y/n): ");
-                    checkgender = Convert.ToChar(Console.ReadLine());
-                    if (checkgender == 'y')
-                    {
-                        Console.Write("Nhap gioi tinh muon doi: ");
-                        xt[i].GioiTinh = Console.ReadLine();
-                    }
-                    else if (checkgender == 'n')
-                    {
-                        Console.WriteLine("Gioi tinh duoc giu nguyen!");
-                    }
-                }
+                Console.Write("Ten sinh vien {0}:", i + 1);
+                Console.WriteLine(sv[i].HoTen);
+                Console.Write("Gioi tinh: ");
+                Console.WriteLine(sv[i].GioiTinh);
+                Console.Write("Que quan: ");
+                Console.WriteLine(sv[i].Que);
+                Console.Write("Hoc luc: ");
+                Console.WriteLine(sv[i].HocLuc);
             }
 
-            //trinh do 5 va la nam thi duoc phep doi thanh sung may, la nu thi duoc doi thanh tieu lien
-            //Doi sung cho xa thu trinh do 5
-            char checkgun;
-            Console.WriteLine("\n*Doi sung cho xa thu co trinh do 5*");
+            //Xoa sinh vien trong mang
+            Console.WriteLine("\n*Xoa sinh vien trong mang*");
+
+            int pos;
+            char check;
+        xoa:
+            pos = 0;
+            //Chuan hoa ten sinh vien can xoa
+            Console.Write("Nhap ten sinh vien can xoa: ");
+            string namep = Console.ReadLine();
+            sv[pos].HoTen = namep;
+                sv[pos].HoTen.Trim();
+                sv[pos].HoTen.ToLower();
+                while (sv[pos].HoTen.IndexOf("  ") != -1)
+                {
+                    sv[pos].HoTen.Remove(sv[pos].HoTen.IndexOf("  "), 1);
+                }
+                string[] p = sv[pos].HoTen.Split(' ');
+                string afterFormatpos = "";
+                for (int j = 0; j < p.Length; ++j)
+                {
+                    string firstpos = p[j].Substring(0, 1);
+                    string anotherpos = p[j].Substring(1, p[j].Length - 1);
+                    afterFormatpos += firstpos.ToUpper() + anotherpos + " ";
+                }
+                sv[pos].HoTen = afterFormatpos.Remove(afterFormatpos.LastIndexOf(' '), 1);
+
+            for (i = 0; i<n; i++)
+            {
+                if (sv[pos].HoTen == sv[i].HoTen)
+                {
+                    sv[i].HoTen = sv[i+1].HoTen;
+                    sv[i].GioiTinh = sv[i+1].GioiTinh;
+                    sv[i].Que = sv[i + 1].Que;
+                    sv[i].HocLuc = sv[i + 1].HocLuc;
+                    n--;
+                }
+                else
+                {
+                    Console.WriteLine("Ten sinh vien khong co tren he thong!");
+                    Console.WriteLine("Ban co muon nhap lai?(y/n): ");
+                    check = Convert.ToChar(Console.ReadLine());
+                    if (check == 'y')
+                        goto xoa;
+                }
+            }
+            Console.Write("\nBan co muon xoa tiep?(y/n): ");
+            check = Convert.ToChar(Console.ReadLine());
+            if (check == 'y')
+                goto xoa;
+                
+
+            //Them sinh vien vao mang
+            Console.WriteLine("\n*Them sinh vien vao he thong*");
+
+        them:
+            Console.Write("Nhap ten sinh vien muon them: ");
+            string nameadd = Console.ReadLine();
+            sv[n-1].HoTen = nameadd;
+                
+            Console.Write("Nhap gioi tinh: ");
+            string genderadd = Console.ReadLine();
+            sv[n-1].GioiTinh = genderadd;
+
+            Console.Write("Nhap que quan: ");
+            string queadd = Console.ReadLine();
+            sv[n-1].Que = queadd;
+
+            Console.Write("Nhap hoc luc: ");
+            string hoclucadd = Console.ReadLine();
+            sv[n-1].HocLuc = hoclucadd;
+
+            Console.Write("\nBan co muon them sinh vien tiep?(y/n): ");
+            check = Convert.ToChar(Console.ReadLine());
+            n++;
+            if (check == 'y')
+                goto them;
+
+            //in thong tin sinh vien
             for (i = 0; i < n; i++)
             {
-                if (xt[i].TD == TrinhDo.ChuyenGia && xt[i].GioiTinh == "nam")
-                {
-                    Console.Write("Ban co muon chon sung may?(y/n): ");
-                    checkgun = Convert.ToChar(Console.ReadLine());
-                    if (checkgun == 'y')
-                        xt[i].LS.TenSung = "Sung May";
-                    else if (checkgun == 'n')
-                        Console.WriteLine("Khong thay doi sung!");
-                }
-                else if (xt[i].TD == TrinhDo.ChuyenGia && xt[i].GioiTinh == "nu")
-                {
-                    Console.Write("Ban co muon chon tieu lien?(y/n): ");
-                    checkgun = Convert.ToChar(Console.ReadLine());
-                    if (checkgun == 'y')
-                        xt[i].LS.TenSung = "Tieu Lien";
-                    else if (checkgun == 'n')
-                        Console.WriteLine("Khong thay doi sung!");
-                }
+                Console.WriteLine("Ten sinh vien {0}: {1}", i + 1, sv[i].HoTen);
+                Console.WriteLine("Gioi tinh: {0}",sv[i].GioiTinh);
+                Console.WriteLine("Que quan: {0}",sv[i].Que);
+                Console.WriteLine("Hoc luc: {0}", sv[i].HocLuc);
             }
-            */
+            Console.ReadKey();
 
-            //In thong tin cac xa thu
-            Console.WriteLine("\n*In thong tin cac xa thu*");
-            for (i = 0; i < n; i++)
-            {
-                Console.WriteLine("Ten xa thu {0}: {1}", i + 1, xt[i].HoTen);
-                Console.WriteLine("Gioi tinh: {0}", xt[i].GioiTinh);
-                Console.WriteLine("Trinh do: {0}", xt[i].TD);
-                Console.WriteLine("Ten sung: {0}", xt[i].LS.TenSung);
-                Console.WriteLine("Ty le: {0}%", xt[i].LS.TyLe);
-                Console.WriteLine("Cu ly: {0}m", xt[i].LS.CuLy);
-                Console.WriteLine("Toc do: {0}m/s", xt[i].LS.TocDo);
-                Console.WriteLine("Diem: {0}", xt[i].Diem);
-                Console.Write("\n");
-            }
-
-            //chuyen mang thanh danh sach
-            Console.WriteLine("\n*Chuyen mang thanh danh sach*\n");
+            //Chuyen mang thanh danh sach
             List<string> Name = new List<string>(n);
-            List<string> Gender = new List<string>(n);
-            List<int> Skill = new List<int>(n);
-            List<string> GunName = new List<string>(n);
-            List<double> Acc = new List<double>(n);
-            List<double> Dis = new List<double>(n);
-            List<double> Spd = new List<double>(n);
-            List<double> Point = new List<double>(n);
+            List<string> Gender = new List<string> (n);
+            List<string> HomeTown = new List<string>(n);
+            List<string> Classify = new List<string>(n);
 
-            for(i = 0; i<n; i++)
+            for(i=0; i<n; i++)
             {
-                Name.Add(xt[i].HoTen);
-                Gender.Add(xt[i].GioiTinh);
-                Skill.Add(Convert.ToInt32(xt[i].TD));
-                GunName.Add(xt[i].LS.TenSung);
-                Acc.Add(xt[i].LS.TyLe);
-                Dis.Add(xt[i].LS.CuLy);
-                Spd.Add(xt[i].LS.TocDo);
-                Point.Add(xt[i].Diem);
+                Name.Add(sv[i].HoTen);
+                Gender.Add(sv[i].GioiTinh);
+                HomeTown.Add(sv[i].Que);
+                Classify.Add(sv[i].HocLuc);
             }
-            Console.WriteLine("{0,-18}{1,-9}{2,-12}{3,-10}{4,-6}{5,-6}{6,-6}{7,-6}", "Ho Ten", "Gioi Tinh", "Trinh Do", "Ten Sung", "Ty Le","Cu Ly","Toc Do","Diem");
-            for(i = 0; i < n;i++)
+
+            Console.WriteLine("{0,-18} {1,-9} {2, -15} {3, -9}", "Ho Ten", "Gioi Tinh", "Que Quan", "Hoc luc");
+            for (i=0; i<n;i++)
             {
-                Console.WriteLine("{0,-18}{1,-9}{2,-12}{3,-10}{4,-6}{5,-6}{6,-6}{7,-6}", Name[i], Gender[i], Skill[i], GunName[i], Acc[i] + "%", Dis[i] + "m", Spd[i]+"m/s", Point[i]);
+                Console.WriteLine("{0,-18} {1,-9} {2, -15} {3, -9}", Name[i], Gender[i], HomeTown[i], Classify[i]);
             }
+
+
 
             Console.ReadKey();
         }
